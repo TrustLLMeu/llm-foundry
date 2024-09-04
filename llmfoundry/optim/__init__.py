b696075a5b0c88dd946fc3a3a8e6ed5eb7d2d360
janEbert <janpublicebert@posteo.net>
2024-09-04 12:07:52 +0200

    Add WSD scheduler to registry

--- llmfoundry/optim/__init__.py
+++ llmfoundry/optim/__init__.py
@@ -3,6 +3,7 @@
 
 from torch.optim import AdamW
 from composer.optim import (
+    ConstantDecayWithWarmupScheduler,
     ConstantWithWarmupScheduler,
     CosineAnnealingWithWarmupScheduler,
     DecoupledAdamW,
@@ -22,6 +23,7 @@ optimizers.register('decoupled_adamw', func=DecoupledAdamW)
 optimizers.register('adamw', func=AdamW)
 optimizers.register('no_op', func=NoOp)
 
+schedulers.register('constant_decay_with_warmup', func=ConstantDecayWithWarmupScheduler)
 schedulers.register('constant_with_warmup', func=ConstantWithWarmupScheduler)
 schedulers.register(
     'cosine_with_warmup',
