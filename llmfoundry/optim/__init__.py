85f78af58f3e98112208eef29cb4033782c1f724
janEbert <janpublicebert@posteo.net>
2024-09-04 12:07:41 +0200

    Add standard AdamW to registry

--- llmfoundry/optim/__init__.py
+++ llmfoundry/optim/__init__.py
@@ -1,6 +1,7 @@
 # Copyright 2022 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
 
+from torch.optim import AdamW
 from composer.optim import (
     ConstantWithWarmupScheduler,
     CosineAnnealingWithWarmupScheduler,
@@ -17,6 +18,7 @@ optimizers.register('adalr_lion', func=DecoupledAdaLRLion)
 optimizers.register('clip_lion', func=DecoupledClipLion)
 optimizers.register('decoupled_lionw', func=DecoupledLionW)
 optimizers.register('decoupled_adamw', func=DecoupledAdamW)
+optimizers.register('adamw', func=AdamW)
 
 schedulers.register('constant_with_warmup', func=ConstantWithWarmupScheduler)
 schedulers.register(
